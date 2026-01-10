2f6fe310c71ac3e026f2d38a6a9f11506445bd69
AphiratNiyom <aphirat.ni.66@ubu.ac.th>
2026-01-10 16:31:35 +0700

    ALLOWED_HOSTS

--- UFAsite/UFAsite/settings.py
+++ UFAsite/UFAsite/settings.py
@@ -18,7 +18,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-!)du$b&m06=2$pz@7n1uz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
+# อนุญาตให้เข้าถึงจากโดเมนของ Render และ localhost
+ALLOWED_HOSTS = ['ufa-deploy.onrender.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
